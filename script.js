82febb591bdcce36805c754f7002f310b3abec44
MurodxojaYunusov <murodxojayunusov492@gmail.com>
2023-05-23 14:17:18 +0500

    sadsad

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name=prompt("Enter your name ")
+if(name){
+    document.getElementById('h2').
+    innerHTML="Welcome "+name;
+}
+function delDate(){
+    alert("You clicked on del data btn")
+    if(confirm("you want to delete heading ")){
+          document.getElementById('h2')
+}
+}
